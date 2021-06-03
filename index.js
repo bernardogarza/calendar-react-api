fa1640784eb73c50dca6a62c8781b3f46bf16038
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-06-03 10:52:50 -0500

    Setted up server and testing endpoint

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+app.listen(4000, () => {
+  console.log(`Server running on port ${4000}`);
+});
