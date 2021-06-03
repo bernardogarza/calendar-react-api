ae7b88e91852db905ab2c6e772c9456e12fdbc96
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-06-03 11:02:59 -0500

    Setted up static site and env variables

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({
-    ok: true,
-  });
-});
+app.use(express.static('public'));
 
-app.listen(4000, () => {
-  console.log(`Server running on port ${4000}`);
+// app.get('/', (req, res) => {
+//   res.json({
+//     ok: true,
+//   });
+// });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
